e3801a7161e0e630acd3f3b9f3b52500bb18aa25
lemonorangeapple <jcj1947725596@hotmail.com>
2024-02-19 20:25:55 +0800

    update

--- Tavin/__init__.py
+++ Tavin/__init__.py
@@ -1,28 +1,23 @@
 import webview
 from flask import *
 
-class webview_api:
-    def __init__(self):
-        self._window = None
-
-    def set_window(self, window):
-        self._window = window
-
-    def quit(self):
-        self._window.destroy()
-
 class Tavin:
     def __init__(self, env):
         self.app = Flask(env)
         self.api = webview_api()
-        self.window = webview.create_window(url = self.app, js_api = self.api)
-        self.route = self.app.route
+        self.window = webview.create_window(url = self.app)
 
     def get_app(self):
         return self.app
 
-    def add_url_rule(self, path, route):
+    def _route(self, route):
+        path = self.path
         self.app.add_url_rule(path, path, route)
+        self.path = ''
+
+    def route(self, path):
+        self.path = path
+        return self._route
 
     def run(self):
         self.api.set_window(self.window)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('README.md', 'r', encoding = 'utf-8') as f:
     long_description = f.read()
 setup(
     name = 'Tavin',
-    version = '1.0.3',
+    version = '1.0.4',
     description = 'A concise application framework.',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
