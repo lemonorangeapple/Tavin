b400dc7549b30f4f5a90b03d14b74ccb1039887c
lemonorangeapple <jcj1947725596@hotmail.com>
2024-02-20 20:21:37 +0800

    1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('README.md', 'r', encoding = 'utf-8') as f:
     long_description = f.read()
 setup(
     name = 'Tavin',
-    version = '1.1.4',
+    version = '1.1.5',
     description = 'A concise application framework.',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
