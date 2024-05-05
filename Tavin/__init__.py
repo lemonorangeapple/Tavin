881a5a9daed822c540f901808f556273fc6bad87
June <107529938+lemonorangeapple@users.noreply.github.com>
2024-05-05 11:21:14 +0800

    Add files via upload

--- Tavin/__init__.py
+++ Tavin/__init__.py
@@ -5,18 +5,10 @@ class Tavin:
     def __init__(self, title, env, **args):
         self.app = Flask(env)
         self.window = webview.create_window(url = self.app, title = title, **args)
+        self.route = self.app.route
 
     def get_app(self):
         return self.app
-
-    def _route(self, route):
-        path = self.path
-        self.app.add_url_rule(path, path, route)
-        self.path = ''
-
-    def route(self, path):
-        self.path = path
-        return self._route
 
     def run(self):
         webview.start()
